207eada39d9c58cc7179ba0982c602b0fdda1611
Tomer Shemesh <tomerjshemesh@gmail.com>
2016-02-09 16:53:18 -0500

    finish stats window Closes #11

--- src/com/chess/Controllers/ViewStatsController.java
+++ src/com/chess/Controllers/ViewStatsController.java
@@ -3,6 +3,8 @@ package com.chess.Controllers;
 import com.chess.Layouts.ViewStatsView;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Tomer on 2/9/16.
@@ -13,9 +15,16 @@ public class ViewStatsController {
 
     }
 
-    public void createView(JFrame frame)
+    public void createView(final JFrame frame)
     {
-        frame.setContentPane(ViewStatsView.getviewStatsView());
+        ActionListener returnAction = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                MainMenuController menu = new MainMenuController();
+                menu.createView(frame);
+            }
+        };
+        frame.setContentPane(ViewStatsView.getviewStatsView(10,5,2,1004,returnAction));
         frame.revalidate();
     }
 

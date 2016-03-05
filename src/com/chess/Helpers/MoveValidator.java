040579fa5d0bad83cb18eff66b98eb562cd643fa
Alex Marion <ajm482@drexel.edu>
2016-03-04 22:44:27 -0500

    refs #38

--- src/com/chess/Helpers/MoveValidator.java
+++ src/com/chess/Helpers/MoveValidator.java
@@ -63,6 +63,9 @@ public class MoveValidator {
     A move forward is -1
      */
 
+    //TODO: check that toCell isn't equal to the fromCell
+    //TODO: check that the toCell is on the board
+
     //================================---------------------================================
     //================================ Validate PAWN moves ================================
     //================================---------------------================================
@@ -257,14 +260,86 @@ public class MoveValidator {
     //================================ Validate ROOK moves ================================
     //================================---------------------================================
     private static boolean validate_ROOK_MOVE(ChessMove move, Board board) {
+        Cell fromCell = move.getFrom();
+        Cell toCell = move.getTo();
+        // rook can move in the column and row
+        // any distance as long as it doesn't
+        // run into another piece
+
+        int moveRow = toCell.getPos().getX();
+        int moveCol = toCell.getPos().getY();
+        int fromRow = fromCell.getPos().getX();
+        int fromCol = fromCell.getPos().getY();
+
+        if (fromCell == toCell)
+            return false;
+        else if (moveRow == fromRow) {
+            int row = fromRow;
+            for(int i = fromCol; i <= moveCol;i--) {
+                if (board.getCell(moveRow, moveCol - 1).getCellState() == Chess.Pieces.EMPTY)
+                    return false;
+            }
+            return true;
+        }
+        else if (moveCol == fromCol) {
+            for(int i = fromRow; i <= moveRow;i--) {
+                if (board.getCell(moveRow - 1, moveCol).getCellState() == Chess.Pieces.EMPTY)
+                    return false;
+            }
+            return true;
+        }
         return false;
     }
 
     private static boolean validate_BLACK_ROOK(ChessMove move, Board board) {
+        Cell fromCell = move.getFrom();
+        Cell toCell = move.getTo();
+
+        int moveRow = toCell.getPos().getX();
+        int moveCol = toCell.getPos().getY();
+        int fromRow = fromCell.getPos().getX();
+        int fromCol = fromCell.getPos().getY();
+
+        if (fromCell == toCell)
+            return false;
+        else if(toCell.getCellState() == Chess.Pieces.EMPTY) {
+            return validate_ROOK_MOVE(move, board);
+        }
+        else if(toCell.getCellState() == Chess.Pieces.WHITE_PAWN
+                || toCell.getCellState() == Chess.Pieces.WHITE_KING
+                || toCell.getCellState() == Chess.Pieces.WHITE_QUEEN
+                || toCell.getCellState() == Chess.Pieces.WHITE_ROOK
+                || toCell.getCellState() == Chess.Pieces.WHITE_KNIGHT
+                || toCell.getCellState() == Chess.Pieces.WHITE_BISHOP) {
+            return validate_ROOK_MOVE(move, board);
+        }
         return false;
     }
 
     private static boolean validate_WHITE_ROOK(ChessMove move, Board board) {
+        Cell fromCell = move.getFrom();
+        Cell toCell = move.getTo();
+
+        int moveRow = toCell.getPos().getX();
+        int moveCol = toCell.getPos().getY();
+        int fromRow = fromCell.getPos().getX();
+        int fromCol = fromCell.getPos().getY();
+
+        if (fromCell == toCell)
+            return false;
+
+        else if(toCell.getCellState() == Chess.Pieces.EMPTY) {
+            return validate_ROOK_MOVE(move, board);
+        }
+
+        else if (toCell.getCellState() == Chess.Pieces.BLACK_PAWN
+                || toCell.getCellState() == Chess.Pieces.BLACK_KING
+                || toCell.getCellState() == Chess.Pieces.BLACK_QUEEN
+                || toCell.getCellState() == Chess.Pieces.BLACK_ROOK
+                || toCell.getCellState() == Chess.Pieces.BLACK_KNIGHT
+                || toCell.getCellState() == Chess.Pieces.BLACK_BISHOP) {
+            return validate_ROOK_MOVE(move, board);
+        }
         return false;
     }
 
@@ -273,14 +348,88 @@ public class MoveValidator {
     //================================ Validate KNIGHT moves ================================
     //================================-----------------------================================
     private static boolean validate_KNIGHT_MOVE(ChessMove move, Board board) {
-        return false;
+        Cell fromCell = move.getFrom();
+        Cell toCell = move.getTo();
+        // rook can move in the column and row
+        // any distance as long as it doesn't
+        // run into another piece
+
+        int toRow = toCell.getPos().getX();
+        int toCol = toCell.getPos().getY();
+        int fromRow = fromCell.getPos().getX();
+        int fromCol = fromCell.getPos().getY();
+
+        if (fromCell == toCell)
+            return false;
+        else if (toRow > 8 || toRow < -1 || toCol > 8 || toCol < -1)
+            return false;
+        else if ((fromCol - 2) == toCol && (fromRow - 1) == toRow)
+            return true;
+        else if ((fromCol - 1) == toCol && (fromRow - 2) == toRow)
+            return true;
+        else if ((fromCol - 2) == toCol && (fromRow + 1) == toRow)
+            return true;
+        else if ((fromRow + 2) == toRow && (fromCol - 1) == toCol)
+            return true;
+        else if ((fromRow - 1) == toRow && (fromCol + 2) == toCol)
+            return true;
+        else if ((fromRow + 1) == toRow && (fromCol + 2) == toCol)
+            return true;
+        else if ((fromRow - 2) == toRow && (fromCol + 1) == toCol)
+            return true;
+        else if ((fromRow + 2) == toRow && (fromCol + 1) == toCol)
+            return true;
+        else
+            return false;
     }
 
     private static boolean validate_BLACK_KNIGHT(ChessMove move, Board board) {
+        Cell fromCell = move.getFrom();
+        Cell toCell = move.getTo();
+
+        int toRow = toCell.getPos().getX();
+        int toCol = toCell.getPos().getY();
+        int fromRow = fromCell.getPos().getX();
+        int fromCol = fromCell.getPos().getY();
+
+        if (fromCell == toCell)
+            return false;
+        else if(toCell.getCellState() == Chess.Pieces.EMPTY) {
+            return validate_KNIGHT_MOVE(move, board);
+        }
+        else if (toCell.getCellState() == Chess.Pieces.WHITE_PAWN
+                || toCell.getCellState() == Chess.Pieces.WHITE_KING
+                || toCell.getCellState() == Chess.Pieces.WHITE_QUEEN
+                || toCell.getCellState() == Chess.Pieces.WHITE_ROOK
+                || toCell.getCellState() == Chess.Pieces.WHITE_KNIGHT
+                || toCell.getCellState() == Chess.Pieces.WHITE_BISHOP) {
+            return validate_KNIGHT_MOVE(move, board);
+        }
         return false;
     }
 
     private static boolean validate_WHITE_KNIGHT(ChessMove move, Board board) {
+        Cell fromCell = move.getFrom();
+        Cell toCell = move.getTo();
+
+        int toRow = toCell.getPos().getX();
+        int toCol = toCell.getPos().getY();
+        int fromRow = fromCell.getPos().getX();
+        int fromCol = fromCell.getPos().getY();
+
+        if (fromCell == toCell)
+            return false;
+        else if(toCell.getCellState() == Chess.Pieces.EMPTY) {
+            return validate_KNIGHT_MOVE(move, board);
+        }
+        else if (toCell.getCellState() == Chess.Pieces.BLACK_PAWN
+                || toCell.getCellState() == Chess.Pieces.BLACK_KING
+                || toCell.getCellState() == Chess.Pieces.BLACK_QUEEN
+                || toCell.getCellState() == Chess.Pieces.BLACK_ROOK
+                || toCell.getCellState() == Chess.Pieces.BLACK_KNIGHT
+                || toCell.getCellState() == Chess.Pieces.BLACK_BISHOP) {
+            return validate_KNIGHT_MOVE(move, board);
+        }
         return false;
     }
 
@@ -288,6 +437,28 @@ public class MoveValidator {
     //================================ Validate BISHOP moves ================================
     //================================-----------------------================================
     private static boolean validate_BISHOP_MOVE(ChessMove move, Board board) {
+        Cell fromCell = move.getFrom();
+        Cell toCell = move.getTo();
+
+        // The difference between the Xs and Ys must be the same
+        int xDiff = toCell.getPos().getX() - fromCell.getPos().getX();
+        int yDiff = toCell.getPos().getY() - fromCell.getPos().getY();
+        int absXDiff = Math.abs(xDiff);
+        int absYDiff = Math.abs(yDiff);
+        if(absXDiff == absYDiff) {
+            // Check that there is no piece in the move path
+            if(xDiff > 0 && yDiff > 0) {
+                for(int i = 0; i < xDiff; i++) {
+
+                }
+            } else if(xDiff > 0 && yDiff < 0) {
+
+            } else if(xDiff < 0 && yDiff > 0) {
+
+            } else if(xDiff < 0 && yDiff < 0) {
+
+            }
+        }
         return false;
     }
 

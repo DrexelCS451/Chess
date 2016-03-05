2c389330acb8f58e147dcbd16fa13bc1cdeff3fb
Alex Marion <ajm482@drexel.edu>
2016-03-05 00:41:09 -0500

    Closes #41

--- src/com/chess/Models/Board.java
+++ src/com/chess/Models/Board.java
@@ -9,9 +9,27 @@ public class Board {
     Cell[][] cells = new Cell[8][8];
     Chess.BoardState state;
 
-    public Board()
-    {
+    public Board() {}
 
+    public Board(String boardString) {
+        String[] cellStrings = boardString.split("\n");
+        String[][] cellLists = new String[cellStrings.length][3];
+
+        for(int i = 0; i < cellStrings.length; i++) {
+            cellLists[i] = cellStrings[i].split(",");
+        }
+
+        int index;
+        int x;
+        int y;
+        for(int i = 0; i < cells.length; i++) {
+            for(int j = 0; j < cells[i].length; j++) {
+                index = (i * cells.length) + j;
+                x = Integer.parseInt(cellLists[index][0]);
+                y = Integer.parseInt(cellLists[index][1]);
+                cells[i][j] = new Cell(new Coordinate(x, y), Chess.Pieces.valueOf(cellLists[index][2]));
+            }
+        }
     }
 
     public Cell getCell(int x, int y)

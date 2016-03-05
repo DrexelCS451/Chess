903eb545c3be0ae698ef915e881114f25cbd73d0
Kelly Shiptoski <krs85@drexel.edu>
2016-03-05 17:53:31 -0500

    Closes #24

--- src/com/chess/Models/Game.java
+++ src/com/chess/Models/Game.java
@@ -1,8 +1,6 @@
 package com.chess.Models;
 
 import com.chess.Helpers.BoardHelper;
-import com.chess.Helpers.MoveValidator;
-import com.chess.Models.Board;
 
 /**
  * Created by AlexMarion on 3/4/16.
@@ -24,8 +22,21 @@ public class Game {
     public Board makeMove(ChessMove m) {
         Coordinate c1 = m.getFrom().getPos();
         Coordinate c2 = m.getTo().getPos();
-        board.getCell(c2).setCellState(board.getCell(c1).getCellState());
-        board.getCell(c1).setCellState(Chess.Pieces.EMPTY);
+        // Promoting a white pawn to a white queen
+        if (board.getCell(c1).getCellState() == Chess.Pieces.WHITE_PAWN && m.getTo().getPos().getY() == 0) {
+            board.getCell(c2).setCellState(Chess.Pieces.WHITE_QUEEN);
+            board.getCell(c1).setCellState(Chess.Pieces.EMPTY);
+        }
+        // Promoting a black pawn to a black queen
+        else if (board.getCell(c1).getCellState() == Chess.Pieces.BLACK_PAWN && m.getTo().getPos().getY() == 7) {
+            board.getCell(c2).setCellState(Chess.Pieces.BLACK_QUEEN);
+            board.getCell(c1).setCellState(Chess.Pieces.EMPTY);
+        }
+        // Normal moves
+        else {
+            board.getCell(c2).setCellState(board.getCell(c1).getCellState());
+            board.getCell(c1).setCellState(Chess.Pieces.EMPTY);
+        }
         return board;
     }
 
